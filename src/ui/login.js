d4af06abacf968d67968ff5527a4c004e8da762a
Daniel Leong <me@dhleong.net>
2017-09-20 00:25:35 +0900

    Add some preliminary validation UI/UX

--- src/ui/login.js
+++ src/ui/login.js
@@ -4,6 +4,44 @@
 
 const blessed = require('blessed');
 
+function autoInput(textarea) {
+    textarea.on('focus', () => {
+        textarea.readInput();
+    });
+    textarea.on('keypress', () => {
+        if (textarea.style.border.fg !== 'black') {
+            textarea.style.border.fg = 'black';
+            textarea.screen.render();
+        }
+    });
+    textarea.on('click', () => {
+        if (!textarea.focused) {
+            textarea.focus();
+        }
+    });
+    textarea.on('submit', () => {
+        textarea.emit('blur')
+        textarea.parent.submit();
+    });
+    return textarea;
+}
+
+function validatePassword(password) {
+    if (!(password && password.length)) return true;
+
+    if (password.length < 6) {
+        return "Passwords must be at least 6 characters";
+    }
+
+    if (password.indexOf(' ') !== -1) {
+        return "Passwords must not contain spaces";
+    }
+}
+
+function validateUsername(username) {
+    if (!(username && username.length)) return true;
+}
+
 class LoginUI {
     constructor(screen) {
         this.screen = screen;
@@ -38,21 +76,21 @@ class LoginUI {
             tags: true,
         });
         form.on('submit', data => {
-            this._resolve(data);
-            delete this._resolve;
-            delete this._reject;
+            // do this on the next tick so keypress handling
+            // happens first (IE: color changes from validation
+            // aren't stepped on by keypress events)
+            process.nextTick(() => {
+                if (!this._validate(data)) return;
+
+                if (this._resolve) {
+                    this._resolve(data);
+                    delete this._resolve;
+                    delete this._reject;
+                }
+            });
         });
 
-        blessed.Text({
-            parent: form,
-            top: 4,
-            left: 0,
-            width: 10,
-            height: 1,
-
-            content: "Username",
-        });
-        const username = blessed.Textbox({
+        const username = this._username = autoInput(blessed.Textbox({
             parent: form,
             top: 3,
             left: 10,
@@ -61,12 +99,19 @@ class LoginUI {
 
             name: 'username',
 
-            inputOnFocus: true,
-
             border: {
                 type: 'line',
             },
-        });
+        }));
+        blessed.Text({
+            parent: form,
+            top: 4,
+            left: 0,
+            width: 10,
+            height: 1,
+
+            content: "Username",
+        }).on('click', () => username.focus());
 
         blessed.Text({
             parent: form,
@@ -77,7 +122,7 @@ class LoginUI {
 
             content: "Password",
         });
-        const password = blessed.Textbox({
+        const password = this._password = autoInput(blessed.Textbox({
             parent: form,
             top: 6,
             left: 10,
@@ -87,12 +132,10 @@ class LoginUI {
             name: 'password',
             censor: true,
 
-            inputOnFocus: true,
-
             border: {
                 type: 'line',
             },
-        });
+        }));
         password.on('submit', () => form.submit());
 
         blessed.Button({
@@ -123,6 +166,31 @@ class LoginUI {
             this._resolve = resolve;
             this._reject = reject;
         });
+    }
+
+    _validate(data) {
+        var dirty = false;
+        const userError = validateUsername(data.username);
+        if (userError) {
+            this._username.style.border.fg = 'red';
+            this._username.focus();
+            dirty = true;
+        }
+
+        const passError = validatePassword(data.password);
+        if (passError) {
+            this._password.style.border.fg = 'red';
+            if (!dirty) {
+                dirty = true;
+                this._password.focus();
+            }
+        }
+
+        if (dirty) {
+            this.screen.render();
+        }
+
+        return !dirty;
     }
 }
 
